40ad799c4cea19ba0182c7fdcc77ee22cc3fd013
epicdevler <dev.epicdevler@gmail.com>
2023-09-08 21:43:33 +0100

    Fixed rendering conflict between next.js and chakra-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,15 @@
-'use client'
 import './globals.css'
 
 import type {Metadata} from 'next'
-import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 import {poppins} from "@/app/fonts";
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
     title: 'Nwadike Philip | epicdevler ',
     description: 'Nwadike Phiip\'s Portfolio',
     keywords: ['Nwadike Philip', 'epicdevler', 'devler', 'cedars', 'android developer', 'android'],
     creator: 'Nwadike Philip (epicdevler)',
 }
 
-const theme = extendTheme({
-    colors: {
-        brand: '#6842EF',
-        white: '#F6F6F6'
-    },
-})
 export default function RootLayout({
                                        children,
                                    }: {
@@ -25,9 +17,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <ChakraProvider theme={theme}>
-            <body className={poppins.variable}>{children}</body>
-        </ChakraProvider>
+
+        <body className={poppins.variable}>{children}</body>
+
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+'use client'
 import HeroSection from "@/app/home/sections/hero/Hero";
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 
+const theme = extendTheme({
+        colors: {
+            brand: '#6842EF',
+            white: '#F6F6F6'
+        },
+    }
+)
 export default function Home() {
-  return (
-      <main>
-          <HeroSection/>
-      </main>
-  )
+    return (
+        <ChakraProvider theme={theme}>
+            <main>
+                <HeroSection/>
+            </main>
+        </ChakraProvider>
+    )
 }
